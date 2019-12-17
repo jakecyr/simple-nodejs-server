caf6773414bb7717f54d7766865c6984ad9ab5f5
Jake Cyr <jake.r.cyr@lmco.com>
2019-12-17 11:11:25 -0500

    Add post request and change no route error message

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // import Simple framework
 const Simple = require('./classes/simple');
 
+const port = process.env.PORT || 8080;
+const host = '0.0.0.0';
+
 // create new simple server with logging
 let app = new Simple(true);
 
 app
     .get('/', (req, res) => {
-        res.end('here')
+        res.end('here');
     })
     .get('/json-test', (req, res) => {
         res.json({ task: 'Task 1' });
     })
-    .listen(8080, '0.0.0.0', () => {
-        console.log('Listening')
+    .post('/add-user', (req, res) => {
+        // database insert statement here //
+        res.json({ success: true, result: 'Added user' });
+    })
+    .listen(port, host, () => {
+        console.log(`Server listening on ${host}:${port}`);
     });
--- classes/simple.js
+++ classes/simple.js
@@ -70,7 +70,7 @@ function Simple(log) {
         if (methodRoutes && methodRoutes[url]) {
             methodRoutes[url](req, res);
         } else {
-            res.json({ error: true, message: 'No route found' })
+            res.json({ success: false, result: `No ${method} route found matching ${url}` })
         }
     }
     /**
