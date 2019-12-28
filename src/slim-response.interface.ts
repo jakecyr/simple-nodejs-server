f5318255c0b41ad035ef81e44f2fb3a400d353e9
rwmartinez <39195392+rwmartinez@users.noreply.github.com>
2019-12-27 22:05:45 -0500

    Reordered interface properties

--- src/slim-response.interface.ts
+++ src/slim-response.interface.ts
@@ -1,7 +1,7 @@
 import { ServerResponse } from 'http';
 
 export interface SlimResponse extends ServerResponse {
-    json: (json: object, statusCode?: number) => void;
     cookie: (name: string, value: string, options?:object) => void;
+    json: (json: object, statusCode?: number) => void;
     writeHead: any;
 }
